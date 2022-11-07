fb131cf8ae5bbe9279c584f90abbfb4588e4be08
ZorfyEvo <nicolasprieur22@gmail.com>
2022-11-07 16:02:31 +0100

    chenille

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express()
 
-app.get('/hello', (req, res) => res.send('Hello Simplonzzz ABC'));
+app.get('/hello', (req, res) => res.send('La cheniiiiiiiiiille'));
 
 app.listen(3009, ()=> console.log("Application démarrée sur le port 3009"));
